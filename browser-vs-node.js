f491e6fdfa1708bfe03d56c491164a40b906e15e
Pandiri Likhita <pandirilikhita@aesl.in>
2026-07-02 18:55:54 +0530

    node-runtime

---
+++ browser-vs-node.js
@@ -0,0 +1,5 @@
+console.log("typeof window:", typeof window);
+console.log("typeof document:", typeof document);
+console.log("typeof process:", typeof process);
+console.log("typeof globalThis:", typeof globalThis);
+console.log("typeof fetch:", typeof fetch);
